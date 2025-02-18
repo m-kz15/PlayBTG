0bff1eabbbe0735bf00aec64a9b70024ead553f9
m-kz15 <154313168+m-kz15@users.noreply.github.com>
2025-02-18 12:09:02 +0900

    Update enchant.js

--- enchant/enchant.js
+++ enchant/enchant.js
@@ -2713,7 +2713,10 @@
             },
             set: function(rotation) {
                 if(this._rotation !== rotation) {
-                    this._rotation = rotation;
+                    /* 20250218 */
+                    if(Math.abs(rotation) > 359) rotation %= 360;
+				    if(rotation < 0) rotation = 360 - Math.abs(rotation);
+                    this._rotation = Math.round(rotation * 10) / 10;
                     this._dirty = true;
                 }
             }
